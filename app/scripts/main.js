768bb2f16df1c112967e6f1db161d067536bf39f
Jeremy Crawford <jcrawford.design@gmail.com>
2014-02-09 23:55:33 -0500

    Third Commit Testing

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -20,6 +20,24 @@ function Collection (models) {
       return result;
     }
   }
+  
+// .add function start •••••••••••••••••••••••••••
+
+  this.add = function (objectLiteral) {
+
+    if (typeof(objectLiteral) != 'object') {
+      throw new Error('All is lost!');
+    }
+
+    // if (_.has(model, 'id') === false) 
+    //   throw new Error("Shit");
+
+    // if (arguments.length > 1)
+    //   throw new Error("Something something something dark side.")
+
+  this.models.push(objectLiteral);
+  }
+
 };
 
 
